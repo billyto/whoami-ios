b3cedf821bbda9c5a30d16c7bd05877d69c8b054
Billy Tobon <billy.tobon@gmail.com>
2013-04-27 12:20:12 -0400

    Initial Commit

---
+++ whoiam/VGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  VGAppDelegate.h
+//  whoiam
+//
+//  Created by Billy Tobon on 4/27/13.
+//  Copyright (c) 2013 viggle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ whoiam/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  whoiam
+//
+//  Created by Billy Tobon on 4/27/13.
+//  Copyright (c) 2013 viggle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "VGAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([VGAppDelegate class]));
+    }
+}
